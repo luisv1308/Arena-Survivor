3bf2495594695ea15678e457156f40ebbb480ea2
Luis Velasquez <l.velasquez1308@gmail.com>
2024-11-13 15:17:27 -0600

    fixed player movement

--- main/objects/player/Player.go
+++ main/objects/player/Player.go
@@ -2,3 +2,14 @@ components {
   id: "Player"
   component: "/main/objects/player/Player.script"
 }
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/objects/player/Barbarian/BarbarianIdle.tilesource\"\n"
+  "}\n"
+  ""
+}
